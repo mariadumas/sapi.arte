c5d1d2710b7ef9442557477801545dab137880d9
mariadumas <dumasmaria@gmail.com>
2022-12-10 19:52:20 -0300

    prints done

--- second_micro/index.js
+++ second_micro/index.js
@@ -31,14 +31,14 @@ app.use("/product", productRoutes)
 
 
 //Para subir a Deta
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 // no need for `app.listen()` on Deta, we run the app automatically.
 module.exports = app; // make sure to export your `app` instance.
 
-// app.listen(process.env.PORT || port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
---
+++ second_micro/public/js/prints.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    let cuadradaSmall = document.querySelector(".cuadrada-small");
+    let cuadradaMedium = document.querySelector(".cuadrada-medium");
+    let rectangularSmall = document.querySelector(".rectangular-small");
+    let rectangularMedium = document.querySelector(".rectangular-medium");
+    let price = document.querySelector(".price")
+
+
+
+
+
+
+    cuadradaSmall.addEventListener("click", function () {
+        price.innerText = "$5000 sin marco | $8000 con marco"
+    })
+    cuadradaMedium.addEventListener("click", function () {
+        price.innerText = "$8000 sin marco | $13 800 con marco" 
+    })
+
+    rectangularSmall.addEventListener("click", function () {
+        price.innerText = "$6000 sin marco | $11 000 con marco"
+    })
+    rectangularMedium.addEventListener("click", function () {
+        price.innerText = "$9000 sin marco | $16 800 con marco"
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
