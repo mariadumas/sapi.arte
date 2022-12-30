bb5aaf62d7b60d00ff68a5809a9cac62022fa8c1
mariadumas <dumasmaria@gmail.com>
2022-12-30 11:11:22 -0300

    new database

--- second_micro/src/database/config/config.js
+++ second_micro/src/database/config/config.js
@@ -1,22 +1,22 @@
 module.exports = {
   "development": {
-    "username": "sql10530996",
-    "password": "hnUGR7VQk9",
-    "database": "sql10530996",
+    "username": "sql10587249",
+    "password": "ZDQZJZg3b3",
+    "database": "sql10587249",
     "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql"
   },
   "test": {
-    "username": "sql10530996",
-    "password": "hnUGR7VQk9",
-    "database": "sql10530996",
+    "username": "sql10587249",
+    "password": "ZDQZJZg3b3",
+    "database": "sql10587249",
     "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "sql10530996",
-    "password": "hnUGR7VQk9",
-    "database": "sql10530996",
+    "username": "sql10587249",
+    "password": "ZDQZJZg3b3",
+    "database": "sql10587249",
     "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql"
   }
